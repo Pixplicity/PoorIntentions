e3c25e7451bd80fbfba5b0198ac787e5edac121f
Paul Lammertsma <paul@pixplicity.com>
2018-06-01 12:19:17 +0200

    initial version explaining the problem, illustrating it and offering a resolution

---
+++ app/src/main/java/com/pixplicity/poorintentions/DetailActivity.kt
@@ -0,0 +1,26 @@
+package com.pixplicity.poorintentions
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ app/src/main/java/com/pixplicity/poorintentions/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.pixplicity.poorintentions
+
+import android.content.ContextWrapper
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.pixplicity.easyprefs.library.Prefs
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val KEY_ENABLE_RESOLUTION = "enable_resolution"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName("prefs")
+                .setUseDefaultSharedPreference(true)
+                .build()
+
+        var enableResolution = Prefs.getBoolean(KEY_ENABLE_RESOLUTION, false)
+
+        if (enableResolution) {
+            // Check that the activity was launched correctly
+            if (!isTaskRoot
+                    && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+                    && intent.action == Intent.ACTION_MAIN) {
+                // This activity doesn't belong; just the launcher or whatever misbehaving
+                Toast.makeText(this, R.string.task_resuming, Toast.LENGTH_SHORT).show()
+                finish()
+                return
+            }
+        }
+
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.setIcon(R.drawable.ic_toolbar_icon)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        sw_resolution.isChecked = enableResolution
+        sw_resolution.setOnCheckedChangeListener { _, isChecked ->
+            enableResolution = isChecked
+            Prefs.putBoolean(KEY_ENABLE_RESOLUTION, enableResolution)
+        }
+
+        bt_continue.setOnClickListener {
+            startActivity(Intent(this, DetailActivity::class.java))
+        }
+    }
+
+}
