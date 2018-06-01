f8a8786bb927357e22dca1ef9822af1d87a9f74d
Paul Lammertsma <paul@pixplicity.com>
2018-06-01 14:35:18 +0200

    renamed package

--- app/src/androidTest/java/com/pixplicity/poorintentions/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/pixplicity/poorintentions/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.pixplicity.steadyintent", appContext.packageName)
+        assertEquals("com.pixplicity.poorintentions", appContext.packageName)
     }
 }
